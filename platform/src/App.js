da44bd9be363f290670e67573c62d6cf42136647
Arnold Amani <arnoldamani08@gmail.com>
2023-12-06 11:06:32 +0300

    Initial commit

--- platform/src/App.js
+++ platform/src/App.js
@@ -1,9 +1,21 @@
 
 import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
+      <RouterProvider router={router} />
       
    </div>
   );
